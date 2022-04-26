fff1cbf0f2873571df146b9df91ea57b901a1e53
HromL <el2523635@gmail.com>
2022-04-26 19:21:12 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log(444);
+const div = document.getElementById('div');
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    div.classList.toggle('hide');
+    console.log('click');
+})
+
+
+div.classList.add('div-green');
+
+console.log(div.className);
+
+// div.style.color = "red";
+
+
