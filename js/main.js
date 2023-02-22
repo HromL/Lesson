ae3fd0925cd3cd2ebae6814c5dcc03c8c98e357f
HromL <el2523635@gmail.com>
2023-02-22 17:19:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-const div = document.getElementById('div');
+const div = document.getElementById("div");
 
-const btn = document.getElementById('btn');
+const btn = document.querySelector(".btn");
 
-btn.addEventListener('click', () => {
-    div.classList.toggle('hide');
-    console.log('click');
-})
+btn.addEventListener("click", () => {
+  div.classList.toggle("hide");
+  console.log("click");
+});
 
-
-div.classList.add('div-green');
+// div.classList.add("div-green");
 
 console.log(div.className);
 
 // div.style.color = "red";
 
-
+const btn1 = document.createElement("button");
+btn1.classList.add("btn");
+div.appendChild(btn1);
+btn1.addEventListener("click", () => {
+  div.classList.toggle("div-red");
+});
